377ee15aa4fcb5f139727fd52c9dda736a0c9a60
proceane <proceane1@gmail.com>
2021-05-26 17:54:08 +0900

    startsWith / endsWith

---
+++ javaDocStudy/src/string/StringStartEndWith.java
@@ -0,0 +1,15 @@
+package string;
+
+public class StringStartEndWith {
+
+	public static void main(String[] args) {
+		String s = "apple banana";
+		System.out.println("s.startsWith(\"app\") : " + s.startsWith("app"));
+		System.out.println("s.startsWith(\"app\", 3) : " + s.startsWith("app", 3));
+		System.out.println("s.startsWith(\"app\", 0) : " + s.startsWith("app", 0));
+		
+		System.out.println("s.endsWith(\"ana\") : " + s.endsWith("ana"));
+
+	}
+
+}
