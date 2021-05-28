9c84cc4cf60000da77dbccde576622ba038e65f4
proceane <proceane1@gmail.com>
2021-05-28 13:22:43 +0900

    charAt

---
+++ javaDocStudy/src/string/StringCharAt.java
@@ -0,0 +1,15 @@
+package string;
+
+public class StringCharAt {
+
+	public static void main(String[] args) {
+		String s = "abcde";
+		for(int i = 0; i < s.length(); i++) {
+			System.out.println(s.charAt(i));
+		}
+		
+		System.out.println(s.charAt(5));
+		
+	}
+
+}
