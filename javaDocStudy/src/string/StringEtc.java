da95b6cab360b411215cd777a50be2f0f5293625
proceane <proceane1@gmail.com>
2021-06-19 13:33:05 +0900

    CASE_INSENSITIVE_ORDER, getBytes, toCharArray 보충

--- javaDocStudy/src/string/StringEtc.java
+++ javaDocStudy/src/string/StringEtc.java
@@ -2,7 +2,11 @@ package string;
 
 import java.io.UnsupportedEncodingException;
 import java.nio.charset.Charset;
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.List;
 
 public class StringEtc {
 
@@ -11,11 +15,36 @@ public class StringEtc {
 		//CASE_INSENSITIVE_ORDER : 대소문자 구분없이 정렬해주는 
 		String[] s = {"a", "Z", "r", "B"};
 		Arrays.sort(s, String.CASE_INSENSITIVE_ORDER);
-		System.out.println(Arrays.toString(s));
+		System.out.println("array : " + Arrays.toString(s));
+		
+		String[] copys = s.clone();
+		Arrays.sort(copys, new Comparator<String>() {
+			//대소문자 상관없이 정렬해야되서 toLowerCase사용
+			@Override
+			public int compare(String o1, String o2) {
+				return o1.toLowerCase().compareTo(o2.toLowerCase());
+			}
+
+			
+		});
+		
+		//System.out.println(Arrays.toString(copys));
+		
+		List<String> strList = new ArrayList<>();
+		strList.add("a");
+		strList.add("Z");
+		strList.add("r");
+		strList.add("B");
+		Collections.sort(strList, String.CASE_INSENSITIVE_ORDER);
+		System.out.println("list : " + strList.toString());
+		
+		System.out.println();
 		
 		//length()
-		String str = "What is the length of this String?";
+		String str = "   What is the length of this String?";
 		System.out.println("This String's length is " + str.length());
+		
+		System.out.println();
 		
 		//isEmpty()
 		String s1 = new String();
@@ -24,11 +53,14 @@ public class StringEtc {
 		String s2 = "";
 		System.out.println("Is s2 Empty? : " + s2.isEmpty());
 		
+		System.out.println();
+		
 		//getBytes()
 		str = "abc";
 		System.out.println("getBytes() : " + Arrays.toString(str.getBytes()));
 		
-		System.out.println("getBytes(Charset charset) : " + Arrays.toString(str.getBytes(Charset.defaultCharset())));
+		System.out.println("getBytes(Charset charset) : " 
+						+ Arrays.toString(str.getBytes(Charset.defaultCharset())));
 		
 		try {
 			System.out.println("getBytes(String charsetName) : " + Arrays.toString(str.getBytes("UTF-8")));
@@ -36,13 +68,26 @@ public class StringEtc {
 			e.printStackTrace();
 		}
 		
+		String korean = "안녕하세요";
+		byte[] koreanByte = korean.getBytes();
+		System.out.println("안녕하세요 -> byte[] : " + Arrays.toString(koreanByte));
+		System.out.println(new String(koreanByte));
+		
+		System.out.println();
+		
+		char[] charArr = str.toCharArray();
+		System.out.println(Arrays.toString(charArr));
+		
+		System.out.println();
+		
 		//hashcode()
 		str = "abc";
 		System.out.println(str.hashCode());
 		
+		System.out.println();
 		//trim()
 		str = "  a      b    c    ";
-		System.out.println(str.trim());
+		System.out.println("시작" + str.trim() + "끝");
 		
 		System.out.println();
 		
