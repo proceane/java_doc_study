88e6fe35ebf71da4f54ca49362558e4ee439f76c
proceane <proceane1@gmail.com>
2021-06-28 19:43:04 +0900

    feat : Integer클래스 정적변수 정리

---
+++ javaDocStudy/src/integer/IntegerField.java
@@ -0,0 +1,23 @@
+package integer;
+
+public class IntegerField {
+
+	public static void main(String[] args) {
+		
+		System.out.println("Integer.BYTES : " + Integer.BYTES);
+		
+		System.out.println();
+		
+		System.out.println("MAX_VALUE : " + Integer.MAX_VALUE);
+		System.out.println("MIN_VALUE : " + Integer.MIN_VALUE);
+		
+		System.out.println();
+		
+		System.out.println("SIZE : " + Integer.SIZE);
+		
+		System.out.println();
+		
+		System.out.println("TYPE : " + Integer.TYPE);
+	}
+
+}
