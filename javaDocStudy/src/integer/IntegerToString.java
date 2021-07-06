b37bb7567a79f19ffd8631848c979a73b891ee45
proceane <proceane1@gmail.com>
2021-07-06 17:11:59 +0900

    feat : Integer toString 메소드 정리

---
+++ javaDocStudy/src/integer/IntegerToString.java
@@ -0,0 +1,17 @@
+package integer;
+
+public class IntegerToString {
+
+	public static void main(String[] args) {
+		int int1 = 123;
+		System.out.println("toString class : " + Integer.toString(int1).getClass());
+		System.out.println("radix 2 : " + Integer.toString(int1, 2));
+		
+		System.out.println();
+		
+		System.out.println("123 toBinaryString : " + Integer.toBinaryString(int1));
+		System.out.println("123 toOctalString : " + Integer.toOctalString(int1));
+		System.out.println("123 toHexString : " + Integer.toHexString(int1));
+	}
+
+}
