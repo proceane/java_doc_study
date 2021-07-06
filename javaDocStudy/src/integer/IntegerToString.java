b012af5f7bac9b2ffeffd4608e47f0fa7a199541
proceane <proceane1@gmail.com>
2021-07-06 17:15:45 +0900

    feat : Integer toUnsignedString 정리

--- javaDocStudy/src/integer/IntegerToString.java
+++ javaDocStudy/src/integer/IntegerToString.java
@@ -12,6 +12,12 @@ public class IntegerToString {
 		System.out.println("123 toBinaryString : " + Integer.toBinaryString(int1));
 		System.out.println("123 toOctalString : " + Integer.toOctalString(int1));
 		System.out.println("123 toHexString : " + Integer.toHexString(int1));
+		
+		System.out.println();
+		
+		int int2 = -111;
+		System.out.println("-111 toUnsignedString : " + Integer.toUnsignedString(int2));
+		System.out.println("-111 toUnsignedString : " + Integer.toUnsignedString(int2, 2));
 	}
 
 }
