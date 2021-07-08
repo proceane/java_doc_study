8a5421be28f566aed42e32af83b767a2e4ebd0a1
proceane <proceane1@gmail.com>
2021-07-08 17:19:30 +0900

    feat : int, long, short, float, doubleValue 정리

---
+++ javaDocStudy/src/integer/IntegerValue.java
@@ -0,0 +1,14 @@
+package integer;
+
+public class IntegerValue {
+
+	public static void main(String[] args) {
+		Integer int1 = 10;
+		System.out.println("intValue : " + int1.intValue());
+		System.out.println("longValue : " + int1.longValue());
+		System.out.println("shortValue : " + int1.shortValue());
+		System.out.println("floatValue : " + int1.floatValue());
+		System.out.println("doubleValue : " + int1.doubleValue());
+	}
+
+}
